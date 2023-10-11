b260550353648ba47401127e8e0618bac9ccf845
christorange <christol.yu@outlook.com>
2023-10-11 05:21:30 -0400

    chore: publish api builds

--- api-export/tsup.config.ts
+++ api-export/tsup.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from 'tsup';
 const tsupConfig = defineConfig({
   entry: ['api-export/index.ts'],
   outDir: 'api-export/dist',
-  format: ['cjs','esm'],
+  format: ['esm'],
   clean: true,
   dts: true,
   tsconfig: 'api-export/tsconfig.tsup.json',
