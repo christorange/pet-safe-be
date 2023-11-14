ed1e649146c0ed9abbbee50134ddd836e3f2bae7
christorange <christol.yu@outlook.com>
2023-11-13 22:42:57 -0500

    cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -19,7 +19,7 @@ export function createServer(opts: ServerOptions) {
   const server = fastify({ logger: dev });
 
   void server.register(cors, {
-    origin: [/\.railway\.app$/, /\.vercel\.app$/],
+    origin: true,
   });
   void server.register(ws);
   void server.register(fastifyTRPCPlugin, {
